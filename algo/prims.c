f381eaa4df3a6ea39fcacfdb7a187a33dd4c6f7e
Anurag Parashar <111885714+anuragparashar26@users.noreply.github.com>
2024-08-08 08:52:11 +0530

    updated

--- algo/prims.c
+++ algo/prims.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
-int visited[10] = {0}, cost[10][10], min, mincost = 0;
-void input(int num) {
-    int i, j;
+
+int i,j,a,b,u,v,n,ne=1;
+int min, mincost=0, visited[10] = {0}, cost[10][10];
+
+int main() {
+    printf("Prim's Algorithm");
+    printf("\nEnter number of nodes: ");
+    scanf("%d", &n);
     printf("Enter adjacency matrix:\n");
-    for(i = 0; i < num; i++) {
-        for(j = 0; j < num; j++) {
+    for(i = 0; i <n; i++) {
+        for(j = 0; j <n; j++) {
             scanf("%d", &cost[i][j]);
-        }
-    }
-}
-
-void prims(int num) {
-    int i, j, ne = 1, a, b, u, v;
-
-    for(i = 0; i < num; i++) {
-        for(j = 0; j < num; j++) {
             if(cost[i][j] == 0) {
                 cost[i][j] = 999;
             }
         }
     }
+    printf("The edges of the min cost spanning tree are:\n");
     visited[0] = 1; 
-    mincost = 0;
-    while(ne < num) {
-        min = 999;
-
-        for(i = 0; i < num; i++) {
+    while(ne < n) {
+        for(i = 0, min=999; i <n; i++) {
             if(visited[i]) {
-                for(j = 0; j < num; j++) {
+                for(j = 0; j <n; j++) {
                     if(!visited[j] && cost[i][j] < min) {
                         min = cost[i][j];
                         a = u = i;
@@ -42,14 +36,5 @@ void prims(int num) {
         cost[a][b] = cost[b][a] = 999; 
     }
     printf("\n\nMinimum cost = %d\n", mincost);
-}
-
-int main() {
-    int num;
-    printf("\n\t\tPrim's Algorithm");
-    printf("\n\nEnter number of nodes: ");
-    scanf("%d", &num);
-    input(num);
-    prims(num);
     return 0;
 }
